4fd8db35cf1c53fec5baf06a3582f55737e121c4
Kova <hugo.vanderhaege@gmail.com>
2025-12-11 20:32:41 +0100

    Initial Spring Boot project setup

---
+++ src/test/java/com/hugo/dogcat/DogcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.dogcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
